9929a2454e7f8626406b97da327fb7bf3cb25671
micke <mikael.leijon@gmail.com>
2022-12-08 15:00:41 +0100

    Day 7 struggle!

--- day7/day7.py
+++ day7/day7.py
@@ -1,23 +1,44 @@
 # from aocutils.clients.client import fetch, submit
-
+from collections import defaultdict
 # fetch(year=2022, day=7)
-def sum_size():
-    pass
-current_dir = ''
+
+
+current_dir = '/'
 dir_sizes = dict()
+dir_sizes['/'] = 'start'
+dir_sizes_tot = defaultdict(lambda: 0)
+size = 0
 if __name__ == '__main__':
-    with open('day7/inputs.txt', 'r') as f:
+    with open('day7/test', 'r') as f:
         for line in f:
+            print(current_dir)
             if line.startswith('$ cd '):
+                dir_sizes[current_dir] = size
                 change_dir = ''.join(line.strip().split()[-1:])
                 if change_dir == '/':
-                    current_dir += '/'
-                    dir_sizes[current_dir] = sum_size(current_dir)
+                    current_dir = '/'
                     continue
                 elif change_dir == '..':
-                    current_dir = current_dir.rsplit('/', 1)[0]
+                    current_dir = current_dir.rsplit('/', 2)[0] + '/'
+                    continue
                 else:
-                    current_dir += '/' + change_dir
+                    current_dir += change_dir + '/'
+                    continue
+            elif line.startswith('$ ls'):
+                size = 0
+                continue
+            elif line.startswith('dir'):
+                continue
+            else:
+                size += int(line.split()[0])
+
+    # for item1 in dir_sizes:
+    #     for item2 in dir_sizes:
+    #         if item2.startswith(item1):
+    #             dir_sizes_tot[item1] += dir_sizes[item2]
+
+    print(dir_sizes)
+
 
 
 # submit(, year=2022, day=7, level=1)
