0d52da0fbb0b00b4b81c312acb095b4983f08232
mleijon <mikael.leijon@gmail.com>
2022-12-09 02:07:18 +0100

    Day 7 done!

--- day7/day7.py
+++ day7/day7.py
@@ -1,45 +1,48 @@
-# from aocutils.clients.client import fetch, submit
+from aocutils.clients.client import fetch, submit
 from collections import defaultdict
-# fetch(year=2022, day=7)
 
-
+fetch(year=2022, day=7)
 current_dir = '/'
 dir_sizes = dict()
-dir_sizes['/'] = 'start'
 dir_sizes_tot = defaultdict(lambda: 0)
-size = 0
+size = -1
 if __name__ == '__main__':
-    with open('day7/test', 'r') as f:
+    with open('day7/inputs.txt', 'r') as f:
         for line in f:
-            print(current_dir)
-            if line.startswith('$ cd '):
-                dir_sizes[current_dir] = size
-                change_dir = ''.join(line.strip().split()[-1:])
-                if change_dir == '/':
-                    current_dir = '/'
-                    continue
-                elif change_dir == '..':
-                    current_dir = current_dir.rsplit('/', 2)[0] + '/'
-                    continue
-                else:
-                    current_dir += change_dir + '/'
-                    continue
-            elif line.startswith('$ ls'):
-                size = 0
-                continue
-            elif line.startswith('dir'):
+            if line.strip() == '$ cd /' and (size == - 1):
                 continue
             else:
-                size += int(line.split()[0])
+                if line.startswith('$ cd '):
+                    dir_sizes[old_dir] = size
+                    change_dir = ''.join(line.strip().split()[-1:])
+                    if change_dir == '..':
+                        current_dir = current_dir.rsplit('/', 2)[0] + '/'
+                    else:
+                        current_dir += change_dir + '/'
+                elif line.startswith('$ ls'):
+                    old_dir = current_dir
+                    size = 0
+                elif line.startswith('dir'):
+                    continue
+                else:
+                    size += int(line.split()[0])
+        dir_sizes[current_dir] = size
+    for item1 in dir_sizes:
+        for item2 in dir_sizes:
+            if item2.startswith(item1):
+                dir_sizes_tot[item1] += dir_sizes[item2]
+    sum_a = 0
+    for item in dir_sizes_tot.values():
+        if item < 100000:
+            sum_a += item
 
-    # for item1 in dir_sizes:
-    #     for item2 in dir_sizes:
-    #         if item2.startswith(item1):
-    #             dir_sizes_tot[item1] += dir_sizes[item2]
-
-    print(dir_sizes)
-
-
-
-# submit(, year=2022, day=7, level=1)
-# submit(, year=2022, day=7, level=2)
+    space_available = 70000000 - dir_sizes_tot['/']
+    space_needed = 30000000 - space_available
+    current_largest = dir_sizes_tot['/']
+    for item in dir_sizes_tot.values():
+        if space_needed <= item < current_largest:
+            current_largest = item
+    print('The answer to part 1 is: {}'.format(sum_a))
+    print('The answer to part 2 is: {}'.format(current_largest))
+    submit(sum_a, year=2022, day=7, level=1)
+    submit(current_largest, year=2022, day=7, level=2)
